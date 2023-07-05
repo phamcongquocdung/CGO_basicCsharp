060a2ed19561fe090d7acebbb33be74617e83fe3
PC_QuocDung <phamcongquocdung2004@gmail.com>
2023-07-05 22:47:40 +0700

    basic

---
+++ bt5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace SystemTime
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("Có nghiệm là: {0}!", solution);
+            }
+
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("Phương trình có vô số nghiệm");
+                }
+                else
+                {
+                    Console.Write("Phương trình vô nghiệm!");
+                }
+            }
+
+        }
+    }
+}
