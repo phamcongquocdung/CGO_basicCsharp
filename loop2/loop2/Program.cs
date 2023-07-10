99b56209f353a9f447beb284fb48d2f9822cb519
PC_QuocDung <phamcongquocdung2004@gmail.com>
2023-07-10 16:32:41 +0700

    loop2

---
+++ loop2/loop2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace loop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(ReadLine());
+            if (number < 2)
+                Write("khong phai so nguyen to");
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    WriteLine(number + " la so nguyen to");
+                }
+                else
+                {
+                    WriteLine(number + " khong la so nguyen ");
+                }
+            }
+
+        }
+    }
+}
