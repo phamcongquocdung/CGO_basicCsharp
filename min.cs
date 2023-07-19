45b85bc2a8aac328d4a3388cba44c5e503803d02
PC_QuocDung <phamcongquocdung2004@gmail.com>
2023-07-19 10:38:29 +0700

    111111111

---
+++ min.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTNN_trong_mang___phuong_thuc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            int []array = { 4, 6, 8, 9, 9 };
+            int index = MinValue(array);
+            Console.WriteLine("Giá Trị Nhỏ Nhất Trong Mảng Là: " + array[index]);
+
+            Console.ReadKey();
+        }
+
+
+        public static int MinValue(int[] array)
+        {
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ nhietdo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGO_Buoi05_ChuyenDOiNhietDo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            double fahrenheit;
+            double celsius;
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Menu.");
+                Console.WriteLine("1. Độ F sang độ C");
+                Console.WriteLine("2. Độ C sang độ F");
+                Console.WriteLine("0. Thoát");
+                Console.WriteLine("Nhập lựa chọn của bạn: ");
+                choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Nhập độ F: ");
+                        fahrenheit = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Độ F sang độ C: " + FahrenheitToCelsius(fahrenheit));
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter Celsius: ");
+                        celsius = Double.Parse(Console.ReadLine());
+                        Console.WriteLine("Độ C sang độ F: " + CelsiusToFahrenheit(celsius));
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                }
+            } while (choice != 0);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            double fahrenheit = (9.0 / 5) * celsius + 32;
+            return fahrenheit;
+        }
+
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            double celsius = (5.0 / 9) * (fahrenheit - 32);
+            return celsius;
+        }
+    }
+}
