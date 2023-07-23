22f48e89f5c17b6beecadd638b4f7d29fde21ecc
PC_QuocDung <phamcongquocdung2004@gmail.com>
2023-07-23 14:29:13 +0700

    sumcheo

---
+++ sumcheoarray/sumcheoarray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace findmaxarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(ReadLine());
+            int sum = 0;
+            int[,] A = new int[100, 100];
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <=n ; j++)
+                {
+                    A[i, j] = int.Parse(ReadLine());
+                }
+            for (int i = 1; i <= n; i++)
+                sum += A[i, i];
+            WriteLine(sum);
+            ReadLine();
+        }
+    }
+}
