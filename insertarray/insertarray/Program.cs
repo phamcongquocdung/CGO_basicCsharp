3583d7ea8b6f843b04f4d51cb5e2d9a91d6b9138
PC_QuocDung <phamcongquocdung2004@gmail.com>
2023-07-23 13:57:04 +0700

    insert

---
+++ insertarray/insertarray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace insertarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> A = new List<int>();
+            int n = int.Parse(ReadLine());
+            for (int i = 1; i <= n; i++)
+                A.Add(int.Parse(ReadLine()));
+            WriteLine("nhap gia tri can chen: ");
+            int gt = int.Parse(ReadLine());
+            WriteLine("nhap vi tri can chen: ");
+            int vt = int.Parse(ReadLine());
+            if (vt <= 1 || vt >= n - 1)
+                WriteLine("khong chen duoc phan tu vao mang");
+            else
+            {
+                A.Insert(vt, gt);
+                for (int i = 0; i <= n - 1; i++)
+                    WriteLine(A[i]);
+            }
+            ReadLine();
+        }
+    }
+}
---
+++ insertarray/insertarray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("insertarray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("insertarray")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d476ec9-ac4c-48e5-a044-3ea0e632850a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
